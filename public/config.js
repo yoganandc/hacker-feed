33068cd1f71a68ceb8b7135410e6756553e4bdfa
Yoganand Chandrasekhar <yoganand1004@gmail.com>
2016-12-07 17:47:05 -0800

    Initial Commit

---
+++ public/config.js
@@ -0,0 +1,9 @@
+(function () {
+    angular
+        .module("HackerFeed")
+        .config(Config)
+
+    function Config($routeProvider) {
+
+    }
+})()
---
+++ server.js
@@ -0,0 +1,46 @@
+if(!process.env.MLAB_USERNAME || !process.env.MLAB_PASSWORD) {
+    throw new Error("mlab username and password not set in environment")
+}
+
+if(!process.env.SESSION_SECRET) {
+    throw new Error("session secret not set in environment")
+}
+
+var mongoose = require("mongoose")
+mongoose.connect("mongodb://" + process.env.MLAB_USERNAME + ":" + process.env.MLAB_PASSWORD + "@ds033106.mlab.com:33106/yoganandc-project")
+var db = mongoose.connection
+
+db.on("error", console.error.bind(console, "could not connect to mlab"))
+
+db.once("open", function() {
+
+    var express = require('express')
+    var app = express()
+
+    var bodyParser = require('body-parser')
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({extended: true}))
+
+    var session = require("express-session")
+    app.use(session({secret: process.env.SESSION_SECRET}))
+
+    var passport = require("passport")
+    app.use(passport.initialize())
+    app.use(passport.session())
+
+    require("./app.js")(app, mongoose, passport)
+    app.use(express.static(__dirname + '/public'))
+    app.use(function(req, res) {
+        res.redirect("/")
+    })
+
+    // use this error handler for all errors that
+    // have not been caught up to this point
+    app.use(function (err, req, res, next) {
+        res.status(400).send({message: "Error"})
+    })
+
+    app.listen(3000)
+    console.log("server started successfully")
+
+})
