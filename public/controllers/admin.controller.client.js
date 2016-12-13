6cb152d804a6a7a05324ef1f36e52f3fe25efa67
Yoganand Chandrasekhar <yoganand1004@gmail.com>
2016-12-13 13:21:05 -0800

    Admin

--- public/controllers/admin.controller.client.js
+++ public/controllers/admin.controller.client.js
@@ -26,13 +26,53 @@
         }
     }
 
-    function AdminSearchController() {
+    function AdminSearchController(UserService) {
         var vm = this
+        vm.search = search
+        vm.updateUser = updateUser
+        vm.openModal = openModal
+        vm.closeModal = closeModal
+        vm.showModal = false
 
         init()
 
         function init() {
+            UserService
+                .loggedIn()
+                .then(function(obj) {
+                    vm.user = obj.data
+                }, function(err) {
+                    console.log(err)
+                })
+        }
 
+        function search() {
+            delete vm.error
+            delete vm.message
+
+            UserService
+                .searchUsersByUsername(vm.query)
+                .then(function (obj) {
+                    vm.results = obj.data
+                    if(vm.results.length === 0) {
+                        vm.message = "No results found."
+                    }
+                }, function (err) {
+                    vm.error = err.data.message
+                })
+        }
+
+        function openModal(userId) {
+            vm.userId = userId
+            vm.showModal = true
+        }
+
+        function closeModal() {
+            vm.showModal = false
+        }
+
+        function updateUser() {
+            vm.showModal = false
         }
     }
 
